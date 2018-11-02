372d5b4f511f694d726a8b264173b49a0d74427b
Nate <nathan.sampo@students.olin.edu>
2018-11-02 13:35:43 -0400

    Generated npz

--- place_soccer_ball.py
+++ place_soccer_ball.py
@@ -1,8 +1,10 @@
+from __future__ import division
+
 from PIL import Image
 import random, math
 import csv
 
-with open('/home/n4tticus/Desktop/Balls/ball_positions.csv', 'wb') as ball_positions:
+with open('/media/n4tticus/ZIEMANN1/Balls/ball_positions.csv', 'wb') as ball_positions:
 	csv_write = csv.writer(ball_positions, quoting=csv.QUOTE_ALL)
 	csv_write.writerow('')
 
@@ -15,23 +17,32 @@ while n < 3000:
 		ball = Image.open('/media/n4tticus/ZIEMANN1/BallOnlyClose.png', 'r')
 		ball_width, ball_height = ball.size
 		relative_height = 125
+		max_angle = 30
+		distance = 70
+		max_horizontal = 40
 	elif n < 2000:
 		ball = Image.open('/media/n4tticus/ZIEMANN1/BallOnlyMid.png', 'r')
 		ball_width, ball_height = ball.size
 		relative_height = 170
+		max_angle = 30
+		distance = 34
+		max_horizontal = 20
 	else:
 		ball = Image.open('/media/n4tticus/ZIEMANN1/BallOnlyFar.png', 'r')
 		ball_width, ball_height = ball.size
 		relative_height = 180
+		max_angle = 30
+		distance = 12
+		max_horizontal = 10
 
 	offset = (random.randint(0, 640 - ball_width), relative_height)
 	angle = random.randint(0, 360)
 	ball = ball.rotate(angle)
 	empty.paste(ball, offset, mask=ball)
 	empty.thumbnail((320, 240), Image.ANTIALIAS)
-	empty.save('/home/n4tticus/Desktop/Balls/' + str(n) + '.png', format='png')
+	empty.save('/media/n4tticus/ZIEMANN1/Balls/' + str(n) + '.png', format='png')
 	n += 1
 
-	with open('/home/n4tticus/Desktop/Balls/ball_positions.csv', 'a') as ball_positions:
+	with open('/media/n4tticus/ZIEMANN1/Balls/ball_positions.csv', 'a') as ball_positions:
 		csv_write = csv.writer(ball_positions, quoting=csv.QUOTE_ALL)
-		csv_write.writerow((offset[0]/2, offset[1]/2, (offset[0] + ball_width)/2, (offset[1] + ball_height)/2))
+		csv_write.writerow((offset[0]/2, offset[1]/2, (offset[0] + ball_width)/2, (offset[1] + ball_height)/2, 2*max_angle*(offset[0]/(640 - ball_width)) - max_angle, math.sqrt((2*max_horizontal*(offset[0]/(640 - ball_width)) - max_horizontal)**2 + distance**2)))
