ca07142d17b12789fae892bfb25a8d54704dffd5
Nate Sampo <nathan.sampo@students.olin.edu>
2018-11-06 04:39:46 -0500

    Added documentation

--- npz.py
+++ npz.py
@@ -7,37 +7,23 @@ notebooks.
 import numpy as np
 import cv2
 
-#created  an empty array to store our images
-images = np.empty((3000, 160, 120, 3), dtype=np.ndarray)
-
-i=0
-#itterate over our all pictures and turning them into an array
-while i < 3000:
-	temp_image = cv2.imread('/media/n4tticus/ZIEMANN1/Balls/' + str(i) + '.png')
-	for j in range(0, 160):
-		for k in range(0, 120):
-			images[i][j][k] = np.array([temp_image[k][j][2], temp_image[k][j][1], temp_image[k][j][0]])
-	i+=1
-
-	if i%100 == 0:
-		print(i)
-#saving  that array
-np.savez('/media/n4tticus/ZIEMANN1/balls.npz', images)
-
-
-'''
+#Create an empty numpy array of length 3000 to hold all of our images
 images = np.empty(3000, dtype=np.ndarray)
 
+#Iterate over the 3000 images and append them to the images list
 for i in range(0, 3000):
+	#Use openCV to read the images as numpy arrays of dimensions (120, 160, 3)
 	images[i] = cv2.imread('/media/n4tticus/ZIEMANN1/Balls/' + str(i) + '.png')
 
+	#Print the progress after every 100th image
 	if i%100 == 0:
 		print(i)
 
+#Save the list of images in the compressed npz format
 np.savez('/media/n4tticus/ZIEMANN1/balls5.npz', images)
-'''
-#^^^ This is the code we actually ran. The other code was slow on google. 
+print('npz created')
 
+#To check that the images were loaded and saved correctly, attempt to load the newly created npz file and print the output
 newnpz = np.load('/media/n4tticus/ZIEMANN1/balls5.npz')
 newarr = newnpz['arr_0']
 print(newarr)
