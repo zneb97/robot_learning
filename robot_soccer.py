3b1e272c80704cba1cf422467ac786d0b32ea7fc
Maxmilian626 <maxmilian.wei@students.olin.edu>
2018-11-05 21:18:19 -0500

    merge conflicts?

--- robot_soccer.py
+++ robot_soccer.py
@@ -2,13 +2,14 @@
 """
 Turn the neato based on learned model
 """
-
+import tensorflow as tf
+from tensorflow.keras import layers
 import rospy
 import pandas
 import math
 import PIL
-import keras
-from keras.models import load_model
+#import keras
+#from keras.models import load_model
 from std_msgs.msg import String
 from tf.transformations import euler_from_quaternion, rotation_matrix, quaternion_from_matrix
 from geometry_msgs.msg import Pose, Twist, Vector3
@@ -23,30 +24,30 @@ class RobotSoccer():
 		self.debugOn = False
 
 		#Robot properties
-	    self.x = 0.0
-	    self.y = 0.0
-	    self.theta = 0.0
-	    self.linVector = Vector3(x=0.0, y=0.0, z=0.0)
-	    self.angVector = Vector3(x=0.0, y=0.0, z=0.0)
-	    self.kp = 1
+		self.x = 0.0
+		self.y = 0.0
+		self.theta = 0.0
+		self.linVector = Vector3(x=0.0, y=0.0, z=0.0)
+		self.angVector = Vector3(x=0.0, y=0.0, z=0.0)
+		self.kp = 1
 
-	    #Getting angle
-	    self.resize = (160, 120)
-	    self.ball_diameter = 7.5 #ball is 7.5 inches in diameter.
-	    self.fov = 60. #Field of view in degrees.
-	    self.focal = 150.*12./self.ball_diameter #The first number is the measured width in pixels of a picture taken at the second number's distance (inches).
-	    self.center = self.resize[0]/2
+		#Getting angle
+		self.resize = (160, 120)
+		self.ball_diameter = 7.5 #ball is 7.5 inches in diameter.
+		self.fov = 60. #Field of view in degrees.
+		self.focal = 150.*12./self.ball_diameter #The first number is the measured width in pixels of a picture taken at the second number's distance (inches).
+		self.center = self.resize[0]/2
 
-        #Image from pi camera
-        self.img = None
+		#Image from pi camera
+		self.img = None
 
-        #ROS
-        # self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=2)
-        # rospy.init_node('tracePolygon')
-        # self.rate = rospy.Rate(2)
+		#ROS
+		# self.pub = rospy.Publisher('/cmd_vel', Twist, queue_size=2)
+		# rospy.init_node('tracePolygon')
+		# self.rate = rospy.Rate(2)
 
-        # rospy.Subscriber("/odom", Odometry, self.setLocation)
-        # rospy.Subscriber("/camera_raw", Image, self.setImage)
+		# rospy.Subscriber("/odom", Odometry, self.setLocation)
+		# rospy.Subscriber("/camera_raw", Image, self.setImage)
 
 
     def publishVelocity(self, linX, angZ):
@@ -116,7 +117,7 @@ class RobotSoccer():
         #Training the model and dataset is now on Google Collabratory:
         #
         #Here we just read in the model from the generated model
-        model = load_model('kerasModel.h5')
+        model = tf.keras.models.load_model('kerasModel.h5')
         return model
 
    	def getAngleDist(x,radius):
@@ -129,55 +130,55 @@ class RobotSoccer():
 	    return angle, difference
 
 
-    def turnToBall(self, ball_theta):
-        """
-        Turn the neato to the soccer ball based on the ball's relative location
-        to the neato
+	def turnToBall(self, ball_theta):
+		"""
+		Turn the neato to the soccer ball based on the ball's relative location
+		to the neato
 
-        Depending on how theta is calculated may have to do some normalization
-        """
+		Depending on how theta is calculated may have to do some normalization
+		"""
 
 		#Determine which way to turn.
 		start_theta = self.theta
-   		ball_theta = -ball_theta
-   		if ball_theta > 1:
-   			angZ = 0.1
-   		elif ball_theta < -1:
-   			angZ = -0.1
+		ball_theta = -ball_theta
+		if ball_theta > 1:
+			angZ = 0.1
+		elif ball_theta < -1:
+			angZ = -0.1
 
-        #Set angle to turn to
-        goal_theta = ball_theta+self.theta
-        if goal_theta > 360:
-            goal_theta = goal_theta-360
-        elif goal_theta < 0:
-            goal_theta = goal_theta+360
+		#Set angle to turn to
+		goal_theta = ball_theta+self.theta
+
+		if goal_theta > 360:
+		    goal_theta = goal_theta-360
+		elif goal_theta < 0:
+		    goal_theta = goal_theta+360
 
 
-        self.publishVelocity(0.0, angZ)
-        while(self.theta < goal_theta):
-            continue
-        self.publishVelocity(0.0,0.0)
+		self.publishVelocity(0.0, angZ)
+		while(self.theta < goal_theta):
+		    continue
+		self.publishVelocity(0.0,0.0)
 
 
     def run(self):
-        useThetaModel = True
+	useThetaModel = True
 
-        #Get the models
-        if useThetaModel:
-            thetaModel = trainThetaModel()
-        else:
-            xyrModel = trainXYRModel()
+	#Get the models
+	if useThetaModel:
+	    thetaModel = trainThetaModel()
+	else:
+	    xyrModel = trainXYRModel()
 
-        #Wait for first image to be obtained
-        while(self.img == None) or (not rospy.is_shutdown()):
-            continue
-
-   		if useThetaModel:
-			#This method is in the model file.
-   			ballTheta = thetaModel.predict(self.img)
-   		else:
-   			ballTheta = xyrModel.predict(self.img)
-   		turnToBall(ballTheta)
+	#Wait for first image to be obtained
+	while(self.img == None) or (not rospy.is_shutdown()):
+	    continue
+	if useThetaModel:
+	#This method is in the model file.
+		ballTheta = thetaModel.predict(self.img)
+	else:
+		ballTheta = xyrModel.predict(self.img)
+	turnToBall(ballTheta)
 
 
 
